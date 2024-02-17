2458e790dad9c84fa440bc36abbd570df7abe8d5
karensof27 <160347099+karensof27@users.noreply.github.com>
2024-02-17 14:38:59 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(800,800);
+}
+
+function draw() {
+background('#F5A6A6');
+  circle(170, 170, 200);
+  circle(500, 170, 200);
+  circle(350, 350, 350);
+  circle(280, 300, 50);
+  circle(430, 295, 50);
+  square(340, 330, 30);
+  
+}
